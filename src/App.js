e791af7e09eca29537e36664ee12cef13987a71e
carlos rojas <carlos.rojas@servinformacion.com>
2018-08-29 10:47:46 -0500

    Reutilizacion de componentes comunes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-
+import { Header } from './components/common';
 class App extends Component {
     render() {
         return (
             <View>
+                <Header headerText="Authentication"/>
                 <Text>
                     Auth app
                 </Text>
