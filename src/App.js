b37ec0de813b57432bbff6fed17cba313a43045a
carlos rojas <carlos.rojas@servinformacion.com>
2018-09-12 14:37:29 -0500

    Se crea componente reutilizable Input.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import firebase from 'firebase';
+import { View } from 'react-native';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyBVh-BPu-V_6PtAalMZClXnMiYHgC-L1b4",
+            authDomain: "authentication-43726.firebaseapp.com",
+            databaseURL: "https://authentication-43726.firebaseio.com",
+            projectId: "authentication-43726",
+            storageBucket: "authentication-43726.appspot.com",
+            messagingSenderId: "696532355234"
+        });
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication"/>
-                <Text>
-                    Auth app
-                </Text>
+                <LoginForm />
             </View>
         );
     }
